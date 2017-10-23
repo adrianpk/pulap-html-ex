82570b908cc920550b55cb3f6eed774a2e8fc1ff
Adrian K <adrianpksw@gmail.com>
2017-10-23 21:25:22 +0200

    Users migration updated.

--- priv/repo/migrations/20171019233900_create_users.exs
+++ priv/repo/migrations/20171019233900_create_users.exs
@@ -3,30 +3,32 @@ defmodule Pulap.Repo.Migrations.CreateUsers do
 
   def up do
     create table(:users, primary_key: false) do
-      add :id, :uuid, primary_key: true
-      add :username, :string
-      add :password, :string
-      add :password_hash, :string
-      add :email, :string
-      add :given_name, :string
-      add :middle_names, :string
-      add :family_name, :string
-      add :card, :jsonb
+      add :id, :binary_id, primary_key: true
+      add :username, :string, size: 32, null: false
+      add :password_hash, :string, size: 128, null: false
+      add :email, :string, size: 255, null: false
+      add :given_name, :string, size: 32
+      add :middle_names, :string, size: 32
+      add :family_name, :string, size: 64
+      add :card, :json
       add :annotations, :jsonb
-      # add :geolocation, :geometry
-      add :started_at, :timestamptz
-      add :created_by, :uuid
-      add :is_active, :boolean, default: false, null: false
-      add :is_logical_deleted, :boolean, default: false, null: false
-
-      timestamps(type: :timestamptz)
     end
 
     execute("SELECT AddGeometryColumn ('users','geolocation',4326,'POINT',2);")
+
+    alter table(:users) do
+      add :created_by, :binary_id
+      add :is_active, :boolean, default: false, null: false
+      add :is_logical_deleted, :boolean, default: false, null: false
+      add :started_at, :timestamptz
+
+      timestamps(type: :timestamptz)
+    end
+    create unique_index(:users, [:username, :email])
+
   end
 
   def down do
     drop table("users")
   end
 end
-
