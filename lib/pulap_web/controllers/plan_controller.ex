af9ed8563409917839956b24424f7064f8057c1c
Adrian K <adrianpksw@gmail.com>
2017-12-04 00:06:11 +0100

    Plan implemented in Pulap.Biz module.

--- lib/pulap_web/controllers/plan_controller.ex
+++ lib/pulap_web/controllers/plan_controller.ex
@@ -2,20 +2,20 @@ defmodule PulapWeb.PlanController do
   use PulapWeb, :controller
 
   alias Pulap.App
-  alias Pulap.App.Plan
+  alias Pulap.Biz.Plan
 
   def index(conn, _params) do
-    plans = App.list_plans()
+    plans = Biz.list_plans()
     render(conn, "index.html", plans: plans)
   end
 
   def new(conn, _params) do
-    changeset = App.change_plan(%Plan{})
+    changeset = Biz.change_plan(%Plan{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"plan" => plan_params}) do
-    case App.create_plan(plan_params) do
+    case Biz.create_plan(plan_params) do
       {:ok, plan} ->
         conn
         |> put_flash(:info, "Plan created successfully.")
@@ -26,20 +26,20 @@ defmodule PulapWeb.PlanController do
   end
 
   def show(conn, %{"id" => id}) do
-    plan = App.get_plan!(id)
+    plan = Biz.get_plan!(id)
     render(conn, "show.html", plan: plan)
   end
 
   def edit(conn, %{"id" => id}) do
-    plan = App.get_plan!(id)
-    changeset = App.change_plan(plan)
+    plan = Biz.get_plan!(id)
+    changeset = Biz.change_plan(plan)
     render(conn, "edit.html", plan: plan, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "plan" => plan_params}) do
-    plan = App.get_plan!(id)
+    plan = Biz.get_plan!(id)
 
-    case App.update_plan(plan, plan_params) do
+    case Biz.update_plan(plan, plan_params) do
       {:ok, plan} ->
         conn
         |> put_flash(:info, "Plan updated successfully.")
@@ -50,8 +50,8 @@ defmodule PulapWeb.PlanController do
   end
 
   def delete(conn, %{"id" => id}) do
-    plan = App.get_plan!(id)
-    {:ok, _plan} = App.delete_plan(plan)
+    plan = Biz.get_plan!(id)
+    {:ok, _plan} = Biz.delete_plan(plan)
 
     conn
     |> put_flash(:info, "Plan deleted successfully.")
