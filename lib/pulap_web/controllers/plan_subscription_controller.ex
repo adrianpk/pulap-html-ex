925145afce926969ab3e3d2e91f6146a94d78e6b
Adrian K <adrianpksw@gmail.com>
2017-12-04 00:02:24 +0100

    Plan Subscription context related functions implemented in Pulap.Biz module.

--- lib/pulap_web/controllers/plan_subscription_controller.ex
+++ lib/pulap_web/controllers/plan_subscription_controller.ex
@@ -2,20 +2,20 @@ defmodule PulapWeb.PlanSubscriptionController do
   use PulapWeb, :controller
 
   alias Pulap.App
-  alias Pulap.App.PlanSubscription
+  alias Pulap.Biz.PlanSubscription
 
   def index(conn, _params) do
-    plan_subscriptions = App.list_plan_subscriptions()
+    plan_subscriptions = Biz.list_plan_subscriptions()
     render(conn, "index.html", plan_subscriptions: plan_subscriptions)
   end
 
   def new(conn, _params) do
-    changeset = App.change_plan_subscription(%PlanSubscription{})
+    changeset = Biz.change_plan_subscription(%PlanSubscription{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"plan_subscription" => plan_subscription_params}) do
-    case App.create_plan_subscription(plan_subscription_params) do
+    case Biz.create_plan_subscription(plan_subscription_params) do
       {:ok, plan_subscription} ->
         conn
         |> put_flash(:info, "Plan subscription created successfully.")
@@ -26,20 +26,20 @@ defmodule PulapWeb.PlanSubscriptionController do
   end
 
   def show(conn, %{"id" => id}) do
-    plan_subscription = App.get_plan_subscription!(id)
+    plan_subscription = Biz.get_plan_subscription!(id)
     render(conn, "show.html", plan_subscription: plan_subscription)
   end
 
   def edit(conn, %{"id" => id}) do
-    plan_subscription = App.get_plan_subscription!(id)
-    changeset = App.change_plan_subscription(plan_subscription)
+    plan_subscription = Biz.get_plan_subscription!(id)
+    changeset = Biz.change_plan_subscription(plan_subscription)
     render(conn, "edit.html", plan_subscription: plan_subscription, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "plan_subscription" => plan_subscription_params}) do
-    plan_subscription = App.get_plan_subscription!(id)
+    plan_subscription = Biz.get_plan_subscription!(id)
 
-    case App.update_plan_subscription(plan_subscription, plan_subscription_params) do
+    case Biz.update_plan_subscription(plan_subscription, plan_subscription_params) do
       {:ok, plan_subscription} ->
         conn
         |> put_flash(:info, "Plan subscription updated successfully.")
@@ -50,8 +50,8 @@ defmodule PulapWeb.PlanSubscriptionController do
   end
 
   def delete(conn, %{"id" => id}) do
-    plan_subscription = App.get_plan_subscription!(id)
-    {:ok, _plan_subscription} = App.delete_plan_subscription(plan_subscription)
+    plan_subscription = Biz.get_plan_subscription!(id)
+    {:ok, _plan_subscription} = Biz.delete_plan_subscription(plan_subscription)
 
     conn
     |> put_flash(:info, "Plan subscription deleted successfully.")
