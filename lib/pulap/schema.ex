24e63e51850a04544d3484baaf8675d1d6af1f33
Adrian K <adrianpksw@gmail.com>
2017-12-21 21:36:08 -0300

    Directive removed.

--- lib/pulap/schema.ex
+++ lib/pulap/schema.ex
@@ -2,8 +2,9 @@ defmodule Pulap.Schema do
   defmacro __using__(_) do
     quote do
       use Ecto.Schema
-      @primary_key {:id, :binary_id, autogenerate: true}
-      @foreign_key_type :binary_id
+      @primary_key {:id, Ecto.UUID, autogenerate: true}
+      @foreign_key_type Ecto.UUID
+      @derive {Phoenix.Param, key: :id}
     end
   end
 end
